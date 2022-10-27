3b990ba725a8c26f2943f104e4a8e923143093a4
Oleg Maizer <zippooleg3@gmail.com>
2022-10-27 03:22:27 +0300

    Added detailed game logs page

--- src/components/GameLogBox.jsx
+++ src/components/GameLogBox.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
 
 export default function GameLogBox({ data }) {
-    
+  
   return (
     <div className='bg-[#1e2633] h-40 md:h-44 w-full flex flex-row space-x-1 items-center'>
     {data.slice(0, 5).map(game => 
         <div className='bg-[#00b9bf] h-32 md:h-36 flex-1 flex flex-col justify-center items-center'>
-            <p className='font-extrabold text-sm'>{game.MATCHUP}</p>
-            <p className='font-extrabold text-sm'>PTS: {game.PTS}</p>
-            <p className='font-extrabold text-sm'>AST: {game.AST}</p>
-            <p className='font-extrabold text-sm'>REB: {game.REB}</p>
-            <p className='font-extrabold text-sm'>MIN: {game.MIN}</p>
-            <p className='font-medium'>{game.WL}</p>
+            <p className='font-extrabold text-xs'>{game.GAME_DATE}</p>
+            <p className='font-extrabold text-xs md:text-sm'>{game.MATCHUP}</p>
+            <p className='font-extrabold text-xs md:text-sm'>PTS: {game.PTS}</p>
+            <p className='font-extrabold text-xs md:text-sm'>AST: {game.AST}</p>
+            <p className='font-extrabold text-xs md:text-sm'>REB: {game.REB}</p>
+            <p className='font-extrabold text-xs md:text-sm'>MIN: {game.MIN}</p>
+            <p className='font-medium' style={{color: game.WL === "W" ? "#00ff4c" : "#ff002d"}}>{game.WL}</p>
         </div>
     )}
 </div>
---
+++ src/components/GameLogsDetailed.jsx
@@ -0,0 +1,84 @@
+
+export default function GameLogsDetailed({ data,closeBtn }) {
+    
+  return (
+    <div className='fixed top-0 md:top-5 bg-white h-full md:h-[calc(100%-1.25rem)] w-full md:w-[calc(100%-2rem)] z-50 shadow-md'>
+        <div className='bg-white py-3 relative'>
+            <h2 className='flex flex-col items-center space-y-0.5'>
+                <span className='text-[#a0aec0] text-xs'>DENI AVDIJA STATS</span>
+                <span className='text-black font-bold text-2xl'>ALL GAME LOGS</span>
+                <span className='text-[#7c8b97] text-sm font-medium'>All Game Stats</span>
+                <p className='w-full pt-3 text-center'>
+                    <span className='bg-[#00b9bf] w-1/2 inline-block p-1 text-white text-sm'>DENI AVDIJA</span>
+                    <span className='bg-[#fe317e] w-1/2 inline-block p-1 text-white text-sm'>WASHINGTON WIZARDS</span>
+                </p>
+            </h2>
+            <button
+                onClick={() => closeBtn(false)}
+                className='bg-[#ffd600] px-6 py-4 absolute top-0 right-0'>
+                xx
+            </button>
+        </div>
+        <div className="overflow-x-auto">
+            <table className="table-auto w-full text-sm text-left text-gray-500">
+                <thead className='"text-xs text-[#4a5568] uppercase bg-gray-300'>
+                    <tr>
+                        <th scope='col' className='py-2 md:py-3 px-5 md:px-6'>GAME DATE</th>
+                        <th scope='col' className='py-2 md:py-3 px-5 md:px-6'>MATCHUP</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>W/L</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>MIN</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>PTS</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>FGM</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>FGA</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>FG%</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>3PM</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>3PA</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>3P%</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>FTM</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>FTA</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>FT%</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>OREB</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>DREB</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>REB</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>AST</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>STL</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>BLK</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>TOV</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>PF</th>
+                        <th scope='col' className='py-2 md:py-3 px-3'>+/-</th>
+                    </tr>
+                </thead>
+                {data.map((game,i) => 
+                <tbody>
+                    <tr className="border-b" style={{backgroundColor: i % 2 === 0 ? "rgb(255,255,255)" : "rgb(209 213 219)"}}>
+                        <td className="py-1 md:py-4 px-5 md:px-6">{game.GAME_DATE}</td>
+                        <td className="py-1 md:py-4 px-5 md:px-6">{game.MATCHUP}</td>
+                        <td className="py-1 md:py-4 px-4">{game.WL}</td>
+                        <td className="py-1 md:py-4 px-4">{game.MIN}</td>
+                        <td className="py-1 md:py-4 px-4">{game.PTS}</td>
+                        <td className="py-1 md:py-4 px-4">{game.FGM}</td>
+                        <td className="py-1 md:py-4 px-4">{game.FGA}</td>
+                        <td className="py-1 md:py-4 px-4">{(game.FG_PCT *100).toFixed(1)}</td>
+                        <td className="py-1 md:py-4 px-4">{game.FG3M}</td>
+                        <td className="py-1 md:py-4 px-4">{game.FG3A}</td>
+                        <td className="py-1 md:py-4 px-4">{(game.FG3_PCT *100).toFixed(1)}</td>
+                        <td className="py-1 md:py-4 px-4">{game.FTM}</td>
+                        <td className="py-1 md:py-4 px-4">{game.FTA}</td>
+                        <td className="py-1 md:py-4 px-4">{(game.FT_PCT *100).toFixed(1)}</td>
+                        <td className="py-1 md:py-4 px-4">{game.OREB}</td>
+                        <td className="py-1 md:py-4 px-4">{game.DREB}</td>
+                        <td className="py-1 md:py-4 px-4">{game.REB}</td>
+                        <td className="py-1 md:py-4 px-4">{game.AST}</td>
+                        <td className="py-1 md:py-4 px-4">{game.STL}</td>
+                        <td className="py-1 md:py-4 px-4">{game.BLK}</td>
+                        <td className="py-1 md:py-4 px-4">{game.TOV}</td>
+                        <td className="py-1 md:py-4 px-4">{game.PF}</td>
+                        <td className="py-1 md:py-4 px-4">{game.PLUS_MINUS}</td>
+                    </tr>
+                </tbody>
+                )}
+            </table>
+        </div>
+    </div>
+  )
+}
